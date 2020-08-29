80f46f7b41aaf234705cb8adae0fdc3ad4e8e863
evaristofm <evaristofm@gmail.com>
2020-08-29 13:47:33 -0300

    Adicionando tests para validar campos id, titulo, descrição e estado das tarefas

--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -20,6 +20,43 @@ def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
     resposta = cliente.get('/tarefas')
     assert isinstance(resposta.json(), list)
 
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
+    TAREFAS.append({"id": 1})
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "id" in resposta.json().pop()
+    TAREFAS.clear()
+
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_titulo():
+    TAREFAS.append({"titulo": "titulo 1"})
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "titulo" in resposta.json().pop()
+    TAREFAS.clear()
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_descricao():
+    TAREFAS.append({"descricao": "descricao 1"})
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "descricao" in resposta.json().pop()
+    TAREFAS.clear()
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_estado():
+    TAREFAS.append({"estado": "finalizado"})
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "estado" in resposta.json().pop()
+    TAREFAS.clear()
+
+
+
+
+
+
+
+
+'''
 def test_deve_listar_tarefas():
     tarefa = {
         "id": 1,
@@ -32,3 +69,4 @@ def test_deve_listar_tarefas():
     resposta = cliente.get('/tarefas')
     assert resposta.json() == [tarefa]
     TAREFAS.clear()
+    '''
